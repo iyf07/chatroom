2435e21793a0a8dcde732e4b9509d6d92fb89830
Ivan Yang <yfyang07@gmail.com>
2023-03-23 11:53:05 -0400

    Implement server broadcast and client read

--- client/main.go
+++ client/main.go
@@ -2,24 +2,116 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"github.com/gookit/color"
+	"io"
+	"log"
 	"net"
 	"os"
 	"strings"
+	time2 "time"
 )
 
-func main(){
-	connection, err := net.Dial("tcp", "localhost:8080")
+func main() {
+	// Parse command-line flag
+	var ip = flag.String("ip", "51", "host-ip")
+	var port = flag.String("port", "8080", "port")
+	var name = flag.String("name", "User", "name")
+	flag.Parse()
+
+	// Get client IP address
+	clientIP, err := getIPAddress()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	defer connection.Close()
-	reader := bufio.NewReader(os.Stdin)
-	username, err := reader.ReadString('\n')
-	if err != nil{
-		fmt.Println(err)
+
+	// Connect to the TCP server
+	serverIP := fmt.Sprintf("192.168.86.%s:%s", *ip, *port)
+	connection, err := net.Dial("tcp", serverIP)
+	if err != nil {
+		log.Fatal(err)
 	}
-	username = strings.Trim(username, " \r\n")
-	welcomemsg := fmt.Sprintf("welcome")
-	fmt.Println(welcomemsg)
+	defer func(connection net.Conn) {
+		err = connection.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(connection)
+
+	fmt.Println("Connected to the TCP server " + serverIP)
+	fmt.Println("Your IP address is " + clientIP)
+
+	message := fmt.Sprintf("Welcome %s(%s)\n", *name, clientIP)
+	_, err = connection.Write([]byte(message))
+	if err != nil {
+		log.Fatal(err)
+	}
+	go read(connection)
+	write(connection, *name, clientIP)
+}
+
+func read(connection net.Conn) {
+	for {
+		reader := bufio.NewReader(connection)
+		message, err := reader.ReadString('\n')
+		if err == io.EOF {
+			err := connection.Close()
+			if err != nil {
+				return
+			}
+			fmt.Println("Connection closed")
+			os.Exit(0)
+		}
+		color.Magenta.Println(message)
+	}
+}
+func write(connection net.Conn, name string, clientIP string) {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		time := time2.Now().String()[11:19]
+		message = fmt.Sprintf("%s %s(%s): %s\n", time, name, clientIP, strings.Trim(message, " \r\n"))
+		_, err = connection.Write([]byte(message))
+		if err != nil {
+			break
+		}
+	}
+}
+
+func getIPAddress() (string, error) {
+	// Get all network interfaces
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	// Loop through the network interfaces and print the IPv4 addresses
+	for _, netInterface := range netInterfaces {
+
+		// Ignore loopback and other non-physical interfaces
+		if netInterface.Flags&net.FlagLoopback != 0 || netInterface.Flags&net.FlagUp == 0 {
+			continue
+		}
+
+		// Get a list of unicast addresses for the interface
+		addrs, err := netInterface.Addrs()
+		if err != nil {
+			fmt.Println("Failed to get addresses for interface", netInterface.Name, ":", err)
+			continue
+		}
+
+		// Loop through the unicast addresses and print the IPv4 addresses
+		for _, addr := range addrs {
+
+			// Check if the address is an IPv4 address
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+	return "", nil
 }
--- server/main.go
+++ server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -11,21 +13,100 @@ var newConnection = make(chan net.Conn)
 var deadConnection = make(chan net.Conn)
 
 func main() {
-	ln, err := net.Listen("tcp", ":8080")
+	// Parse command-line flag
+	var port = flag.String("port", "8080", "port")
+	flag.Parse()
+
+	// Get server IP address
+	serverIP, err := getIPAddress()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 
-go func () {
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Panic(err)
+	// Create server
+	ln, err := net.Listen("tcp", ":"+*port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("TCP server listens on " + serverIP + ":" + *port)
+
+	// Accept incoming connection requests
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Fatal(err)
+			}
+			openConnections[conn] = true
+			newConnection <- conn
 		}
-		openConnections[conn] = true
-		newConnection <- conn
-	}
-}()
+	}()
 
-fmt.Println(<- newConnection)
+	// Listen for messages on channels
+	for {
+		select {
+		case conn := <-newConnection:
+			go broadcastMessage(conn)
+
+		case conn := <-deadConnection:
+			for item := range openConnections {
+				if item == conn {
+					break
+				}
+			}
+			delete(openConnections, conn)
+		}
+	}
+}
+
+func broadcastMessage(conn net.Conn) {
+	for {
+		reader := bufio.NewReader(conn)
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		for openConnection := range openConnections {
+			_, err := openConnection.Write([]byte(message))
+			if err != nil {
+				break
+			}
+		}
+	}
+	deadConnection <- conn
+}
+
+func getIPAddress() (string, error) {
+	// Get all network interfaces
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	// Loop through the network interfaces and print the IPv4 addresses
+	for _, netInterface := range netInterfaces {
+
+		// Ignore loopback and other non-physical interfaces
+		if netInterface.Flags&net.FlagLoopback != 0 || netInterface.Flags&net.FlagUp == 0 {
+			continue
+		}
+
+		// Get a list of unicast addresses for the interface
+		addrs, err := netInterface.Addrs()
+		if err != nil {
+			fmt.Println("Failed to get addresses for interface", netInterface.Name, ":", err)
+			continue
+		}
+
+		// Loop through the unicast addresses and print the IPv4 addresses
+		for _, addr := range addrs {
+
+			// Check if the address is an IPv4 address
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+	return "", nil
 }
